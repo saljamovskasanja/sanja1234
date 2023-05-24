71fdd7acc15388cd55d52a9238b1c463b7f0ca58
saljamovskasanja <saljamovskasanja@gmail.com>
2023-05-25 00:00:13 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	sayHello("Ljupce");
+    }
+    public static void sayHello(String name){
+        System.out.println("Helllo " +name);
+    }
+    private int age = 7;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
